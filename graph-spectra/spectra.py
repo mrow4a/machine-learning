e7b521f0d04a5fceca653cee2d59386ca6559849
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-11-29 21:21:51 +0100

    graph spectra on eigenvalues

--- graph-spectra/spectra.py
+++ graph-spectra/spectra.py
@@ -6,9 +6,24 @@ import getopt
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.sparse import csgraph
+from numpy import linalg as LA
+from sklearn.cluster import KMeans
 
 def print_help():
-    print ('./spectra.py filepath')
+    print ('./spectra.py file_path number_of_clusters')
+    print ('./spectra.py /home/ubuntu/edges.dat 4')
+
+def print_edges(edges):
+    # Convert to full matrix
+    n = max(max(left, right) for left, right, weight in edges)  # Get size of matrix
+    matrix = [[0] * n for i in range(n)]
+    for left, right, weight in edges:
+        matrix[left - 1][right - 1] = weight  # Convert to 0-based index.
+
+    A = np.matrix(matrix)
+    plt.matshow(A)
+    plt.show()
 
 def main(argv):
     try:
@@ -17,10 +32,11 @@ def main(argv):
         print_help()
         sys.exit(2)
 
-    if (len(args)) < 1:
+    if (len(args)) < 2:
         print_help()
         sys.exit(2)
     path = args[0]
+    number_of_clusters = int(args[1])
 
     # Read file
     with open(path, 'r') as f:
@@ -35,19 +51,48 @@ def main(argv):
         if len(edges[i]) == 2:
             edges[i].append(1)
 
-    # Convert to full matrix
-    n = max(max(left, right) for left, right, weight in edges)  # Get size of matrix
-    matrix = [[0] * n for i in range(n)]
-    for left, right, weight in edges:
-        matrix[left - 1][right - 1] = weight  # Convert to 0-based index.
+    # Print edges matrix
+    #print_edges(edges)
 
-    A = np.matrix(matrix)
-    # Print full matrix
+    # Start spectral clustering - https://calculatedcontent.com/2012/10/09/spectral-clustering/
 
-    # Display matrix
-    plt.matshow(A)
+    # Calculate affinity matrix and diagonal
+    n = len(edges)
+    affinity_matrix = [[0] * n for i in range(n)]
+    for i in range(0, n):
+        # Create affinity matrix
+        for j in range(0, n):
+            affinity = 0
+            if i != j:
+                pointI= edges[i]
+                pointJ = edges[j]
+                dist = np.sqrt(np.power((pointI[0] - pointJ[0]), 2) + np.power((pointI[1] - pointJ[1]), 2))
+                sigma = 1
+                affinity = np.exp(-1 * np.power(dist, 2) / (2*np.power(sigma, 2)))
 
-    plt.show()
+            affinity_matrix[i][j] = affinity
+
+    # Get Normalized Laplacian from affinity matrix
+    laplacian = csgraph.laplacian(np.matrix(affinity_matrix), normed=True)
+
+    # Get Eigenvectors in ascending order (the normalized (unit length) eigenvectors)
+    eigenvalues, norm_eigenvectors = LA.eigh(laplacian)
+
+    # Get k largest eigenvectors by its eigenvalues
+    kth_largest_eigenvectors = norm_eigenvectors[len(norm_eigenvectors)-number_of_clusters-1:len(norm_eigenvectors)-1]
+
+    eigenvector_points = [[0] * 4 for i in range(n)]
+    for k in range(0, len(kth_largest_eigenvectors)):
+        eigenvector = kth_largest_eigenvectors[k]
+        for i in range(0, len(eigenvector)):
+            eigenvector_points[i][k] = eigenvector[i]
+
+    kmeans_model_labels = KMeans(n_clusters=number_of_clusters).fit(eigenvector_points).labels_
+
+    for i in range(0, len(kmeans_model_labels)):
+        edges[i][2] = kmeans_model_labels[i]
+
+    print_edges(edges)
 
 
 if __name__ == '__main__':
